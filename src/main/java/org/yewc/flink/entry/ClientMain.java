d23cd89d04e3bca2fc5cf6737afcdc1e6ce65f40
barneyye(叶伟成) <barneyye@kugou.net>
2020-03-13 17:10:29 +0800

    first commit

--- src/main/java/org/yewc/flink/entry/ClientMain.java
+++ src/main/java/org/yewc/flink/entry/ClientMain.java
@@ -5,8 +5,8 @@ import org.yewc.flink.netty.FlinkNettyServer;
 public class ClientMain {
 
     public static void main(String[] args) throws Exception {
-//        int port = Integer.valueOf(args[0]);
-        int port = 22223;
+        int port = Integer.valueOf(args[0]);
+//        int port = 22223;
         FlinkNettyServer.start(port);
     }
 
