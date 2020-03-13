d7dcea5061a4b3a3cf0759b9958ff56546439e4d
barneyye(叶伟成) <barneyye@kugou.net>
2020-03-13 16:46:05 +0800

    first commit

--- src/test/java/org/yewc/test/client/FlinkNettyClient.java
+++ src/test/java/org/yewc/test/client/FlinkNettyClient.java
@@ -9,6 +9,7 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.yewc.test.netty.TestHttpClientHandler;
@@ -42,11 +43,10 @@ public class FlinkNettyClient {
                     .handler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         public void initChannel(SocketChannel ch) throws Exception {
-                            ChannelPipeline p = ch.pipeline();
-                            p.addLast("framedecoder",
-                                    new DelimiterBasedFrameDecoder(65535, false,
-                                            Unpooled.copiedBuffer(new byte[]{-25})));
-                            p.addLast(new HttpClientHandler());
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 2, 0, 2));
+                            pipeline.addLast(new LengthFieldPrepender(2));
+                            pipeline.addLast(new HttpClientHandler());
                         }
                     });
 
